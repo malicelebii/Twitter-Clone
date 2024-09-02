444b608227bd0b3d1e8193feb56988862bdd824e
malicelebii <mehmetalicelebii@gmail.com>
2024-09-02 14:32:54 +0300

    + Create ProfileViewController file

---
+++ Twitter-Clone/Modules/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Twitter-Clone
+//
+//  Created by Mehmet Ali ÇELEBİ on 2.09.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Profile"
+    }
+    
+}
