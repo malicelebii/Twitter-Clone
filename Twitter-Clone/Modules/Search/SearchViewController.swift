831f0a38304b3905c0c576b341cd668e83f4a1ca
malicelebii <mehmetalicelebii@gmail.com>
2024-09-02 11:57:32 +0300

    + Create tab views' controllers

---
+++ Twitter-Clone/Modules/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Twitter-Clone
+//
+//  Created by Mehmet Ali ÇELEBİ on 2.09.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
