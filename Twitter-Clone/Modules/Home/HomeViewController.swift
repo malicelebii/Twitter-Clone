990877a437fa0b45a0ff97b81826926f0fb77af3
malicelebii <mehmetalicelebii@gmail.com>
2024-09-02 12:23:38 +0300

    + Basic setup of timeLineTableView for tweets

--- Twitter-Clone/Modules/Home/HomeViewController.swift
+++ Twitter-Clone/Modules/Home/HomeViewController.swift
@@ -9,10 +9,43 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let timelineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addSubViews()
+        setupTimeLineTableView()
     }
     
+    func addSubViews() {
+        view.addSubview(timelineTableView)
+    }
+    
+    func setupTimeLineTableView() {
+        timelineTableView.dataSource = self
+        timelineTableView.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        timelineTableView.frame = view.frame
+    }
+}
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Tweet"
+        cell.contentConfiguration = content
+        return cell
+    }
 }
