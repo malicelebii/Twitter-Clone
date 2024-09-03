fbdf42e9832bbc444b03f9284bac55a03bc3d72a
malicelebii <mehmetalicelebii@gmail.com>
2024-09-03 15:35:43 +0300

    + Create RegisterViewModel file inherits from observable object + Create published email, password, isregistrationforvalid variables + Create isValidEamil method to validate email with regex + Create validateRegistrationForm method to validate form

---
+++ Twitter-Clone/Modules/Register/RegisterViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewModel.swift
+//  Twitter-Clone
+//
+//  Created by Mehmet Ali ÇELEBİ on 3.09.2024.
+//
+
+import Foundation
+
+final class RegisterViewModel: ObservableObject {
+    @Published var email: String?
+    @Published var password: String?
+    @Published var isRegistrationFormValid: Bool = false
+    
+    func validateRegistrationForm() {
+        guard let email = email, let password = password else { return }
+        isRegistrationFormValid = isValidEmail(email) && password.count >= 8
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
