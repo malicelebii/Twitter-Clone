16e98f7ee3d9f1f2f6da4e0f3e9235d4634b46bd
malicelebii <mehmetalicelebii@gmail.com>
2024-09-06 10:23:47 +0300

    + Avoid skip onboarding

--- Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewController.swift
+++ Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewController.swift
@@ -14,6 +14,7 @@ class ProfileDataFormViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        isModalInPresentation = true
     }
    
 }
