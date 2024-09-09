4ceccb1aa4d6fe899614cf57c7df3fade4e0495a
malicelebii <mehmetalicelebii@gmail.com>
2024-09-09 17:11:24 +0300

    + Inject StorageManager while initialization

--- Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
+++ Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
@@ -12,12 +12,19 @@ protocol ProfileDataFormViewViewModelDelegate {
     func uploadAvatar()
 }
 
+final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewViewModelDelegate {
     @Published var displayName: String?
     @Published var username: String?
     @Published var bio: String?
     @Published var avatarPath: String?
     @Published var imageData: UIImage?
     @Published var isFormValid: Bool = false
+    
+    let storageManager: StorageManagerDelegate
+    
+    init(storageManager: StorageManagerDelegate = StorageManager.shared) {
+        self.storageManager = storageManager
+    }
     
     func validateUserProfileForm() {
         guard let displayName, let username, username.count > 2, let bio, bio.count > 2, imageData != nil, displayName.count > 2 else { isFormValid = false ; return }
