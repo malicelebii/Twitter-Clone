d7c88087e729ac6d95e6025bb37fc44ecbf4ee2e
malicelebii <mehmetalicelebii@gmail.com>
2024-09-09 17:13:19 +0300

    + Implement uploadAvatar method by calling manager's uploadProfilePicture method

--- Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
+++ Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import Combine
 
 protocol ProfileDataFormViewViewModelDelegate {
     func validateUserProfileForm()
@@ -13,12 +15,15 @@ protocol ProfileDataFormViewViewModelDelegate {
 }
 
 final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewViewModelDelegate {
+    var subscriptions: Set<AnyCancellable> = []
     @Published var displayName: String?
     @Published var username: String?
     @Published var bio: String?
     @Published var avatarPath: String?
     @Published var imageData: UIImage?
     @Published var isFormValid: Bool = false
+    @Published var url: URL?
+    @Published var error: String?
     
     let storageManager: StorageManagerDelegate
     
@@ -29,5 +34,33 @@ final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewV
     func validateUserProfileForm() {
         guard let displayName, let username, username.count > 2, let bio, bio.count > 2, imageData != nil, displayName.count > 2 else { isFormValid = false ; return }
         isFormValid = true
+    }
+    
+    func uploadAvatar() {
+        let randomID = UUID().uuidString
+        guard let imageData = imageData?.jpegData(compressionQuality: 0.5) else { return }
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        storageManager.uploadProfilePhoto(with: randomID, image: imageData, metaData: metaData)
+            .flatMap({ metaData in
+                self.storageManager.getDownloadURL(for: metaData.path)
+                        })
+                        .sink { [weak self] completion in
+                            
+                            switch completion {
+                            case .failure(let error):
+                                print(error.localizedDescription)
+                                self?.error = error.localizedDescription
+                            
+                            case .finished:
+                                1
+
+                            }
+                            
+                        } receiveValue: { [weak self] url in
+                            self?.avatarPath = url.absoluteString
+                        }
+                        .store(in: &subscriptions)
+
     }
 }
