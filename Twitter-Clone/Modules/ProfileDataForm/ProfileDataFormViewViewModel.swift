3d70c6e8d3e5c71d76fdf106b986a05a9255c8a0
malicelebii <mehmetalicelebii@gmail.com>
2024-09-10 15:28:23 +0300

    + Write updateUserData method to by calling updateUserFields method from databaseManager + Create published var isOnboardingFinished and watch it

--- Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
+++ Twitter-Clone/Modules/ProfileDataForm/ProfileDataFormViewViewModel.swift
@@ -8,10 +8,12 @@
 import UIKit
 import FirebaseStorage
 import Combine
+import FirebaseAuth
 
 protocol ProfileDataFormViewViewModelDelegate {
     func validateUserProfileForm()
     func uploadAvatar()
+    func updateUserData()
 }
 
 final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewViewModelDelegate {
@@ -22,6 +24,7 @@ final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewV
     @Published var avatarPath: String?
     @Published var imageData: UIImage?
     @Published var isFormValid: Bool = false
+    @Published var isOnboardingFinished: Bool = false
     @Published var url: URL?
     @Published var error: String?
     
@@ -53,16 +56,39 @@ final class ProfileDataFormViewViewModel: ObservableObject, ProfileDataFormViewV
                             case .failure(let error):
                                 print(error.localizedDescription)
                                 self?.error = error.localizedDescription
-                            
+                                
                             case .finished:
-                                1
-
+                                self?.updateUserData()
                             }
                             
                         } receiveValue: { [weak self] url in
                             self?.avatarPath = url.absoluteString
                         }
                         .store(in: &subscriptions)
+
+    }
+    
+    func updateUserData() {
+        guard let displayName, let username, let bio, let avatarPath, let id = Auth.auth().currentUser?.uid else { return }
+        
+        let updatedFields: [String: Any] = [
+            "displayName": displayName,
+            "username": username,
+            "bio": bio,
+            "avatarPath": avatarPath,
+            "isUserOnboarded": true
+        ]
+        
+        databaseManager.updateUserFields(fields: updatedFields, for: id )
+            .sink {[weak self] completion in
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                    self?.error = error.localizedDescription
+                }
+            } receiveValue: {[weak self] updated in
+                self?.isOnboardingFinished = updated
+            }
+            .store(in: &subscriptions)
 
     }
 }
