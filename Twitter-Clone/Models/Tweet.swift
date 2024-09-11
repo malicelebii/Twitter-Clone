b14a4b39b5178c03ed980326ab883e7525837a16
malicelebii <mehmetalicelebii@gmail.com>
2024-09-11 17:01:08 +0300

    + Create Tweet Model

---
+++ Twitter-Clone/Models/Tweet.swift
@@ -0,0 +1,19 @@
+//
+//  Tweet.swift
+//  Twitter-Clone
+//
+//  Created by Mehmet Ali ÇELEBİ on 11.09.2024.
+//
+
+import Foundation
+
+struct Tweet: Codable, Identifiable {
+    var id = UUID().uuidString
+    let author: TwitterUser
+    let authorID: String
+    let tweetContent: String
+    var likesCount: Int
+    var likers: [String]
+    let isReply: Bool
+    let parentReference: String?
+}
