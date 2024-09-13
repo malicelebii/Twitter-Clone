50d6799617a416abb25b84024fdacc5e1e37b475
malicelebii <mehmetalicelebii@gmail.com>
2024-09-13 17:11:05 +0300

    + Complete search logic

--- Twitter-Clone/Modules/Search/SearchViewController.swift
+++ Twitter-Clone/Modules/Search/SearchViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let searchViewModel = SearchViewModel()
+
+    let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: SearchResultsViewController())
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.searchBar.placeholder = "Search with @username"
+        return searchController
+    }()
+    
+    let promptLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Search for users and get connected"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .placeholderText
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(promptLabel)
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        configureConstraint()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureConstraint() {
+        NSLayoutConstraint.activate([
+            promptLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            promptLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -20),
+            promptLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
-    */
+}
 
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultsViewController = searchController.searchResultsController as? SearchResultsViewController, let query = searchController.searchBar.text else { return }
+        searchViewModel.search(with: query) { users in
+            resultsViewController.update(users: users)
+        }
+       
+    }
 }
